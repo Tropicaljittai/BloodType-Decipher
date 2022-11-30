9a8059dcaafdeea700f1428eafb65cd2a2ced597
Tropicaljittai <93302274+Tropicaljittai@users.noreply.github.com>
2022-11-30 11:04:59 +0700

    Add files via upload

---
+++ BloodType.js
@@ -0,0 +1,69 @@
+function myFunction(b1, b2) {
+    if (b1.charAt(1) == "-"){
+      if(b1.charAt(0) == "O"){
+        return true;
+      }
+      else if(b1.charAt(0) == "A"){
+        if (b2.charAt(0) == "A" || b2.charAt(1) == "B"){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }
+      else if(b1.charAt(0) == "B"){
+        if (b2.charAt(0) == "B" || b2.charAt(1) == "B"){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }
+  }
+  else if (b1.charAt(1) == "+"){
+    if(b1.charAt(0) == "O"){
+      if (b2.charAt(1) == "+" || b2.charAt(2) == "+"){
+      return true;
+    }
+    else{
+      return false;
+    }
+    }
+    else if(b1.charAt(0) == "A"){
+      if (b2 == "A+" || b2 == "AB+"){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else if(b1.charAt(0) == "B"){
+      if (b2 == "B+" || b2 == "AB+"){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+  else if (b1 == "AB-" || b1 == "AB+"){
+    if (b1 == "AB-"){
+      if (b2 == "AB+" || b2 == "AB-"){
+              return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else if (b1 == "AB+"){
+      if (b2 == "AB+"){
+        return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+}
+var f = myFunction("AB+", "A+");
+console.log(f);
