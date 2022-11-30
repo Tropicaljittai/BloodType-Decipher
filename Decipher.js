6957f0358022887603b2ac0312cb1880bbd37232
Tropicaljittai <93302274+Tropicaljittai@users.noreply.github.com>
2022-11-30 12:28:57 +0700

    Update Decipher.js

--- Decipher.js
+++ Decipher.js
@@ -1,4 +1,4 @@
-function myFunction(b1, b2) {
+function digitalDecipher(b1, b2) {
     const key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var result = ""
     const keyarr = Array.from(String(b2), Number);
@@ -7,4 +7,6 @@ function myFunction(b1, b2) {
     }
     console.log(result);
 }
-f = myFunction([6, 4, 1, 3, 9, 20], 100);
+digitalDecipher([20, 12, 18, 30, 21], 1939);
+digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990);
+digitalDecipher([6, 4, 1, 3, 9, 20], 100);
